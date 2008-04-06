d4fd4122cc3801ae55523d3f856e1b9768152502
Daniel Önnerby <onnerby@gmail.com>
2008-04-06 22:10:36 +0000

    Fixing some issues with old id3v1 genre numbers in some rare mp3s. Added a generic way of getting all of taglibs supported files.

--- src/contrib/taglib_plugin/TagReaderTaglib.h
+++ src/contrib/taglib_plugin/TagReaderTaglib.h
@@ -38,12 +38,15 @@
 
 #include "stdafx.h"
 
-#include <tlist.h>
-#include <fileref.h>
-#include <tfile.h>
-#include <tag.h>
-#include <mpegfile.h>
-#include <id3v2tag.h>
+#include <toolkit/tlist.h>
+#include <toolkit/tfile.h>
+
+#include <taglib/tag.h>
+#include <taglib/fileref.h>
+#include <taglib/audioproperties.h>
+
+#include <mpeg/id3v2/id3v2tag.h>
+
 
 #include <set>
 
@@ -54,15 +57,22 @@ class TagReaderTaglib : public musik::core::Plugin::IMetaDataReader {
 		TagReaderTaglib(void);
 		virtual ~TagReaderTaglib(void);
 		bool ReadTag(musik::core::Track *track);
-		virtual bool CanReadTag(const wchar_t *extension);
+		virtual bool CanReadTag(const utfchar *extension);
         virtual void Destroy();
 	private:
 //		bool getStandardTags(musik::core::Track &track,TagLib::Tag *oTag,TagLib::AudioProperties *oAudioProperties);
 
 		void SetTagValue(const char* key,const wchar_t* string,musik::core::Track *track);
 		void SetTagValue(const char* key,const TagLib::String tagString,musik::core::Track *track);
+		void SetTagValue(const char* key,const int tagInt,musik::core::Track *track);
 		void SetTagValues(const char* key,const TagLib::ID3v2::FrameList &frame,musik::core::Track *track);
+        void SetAudioProperties(TagLib::AudioProperties *audioProperties,musik::core::Track *track);
 
 		void SetSlashSeparatedValues(const char* key,const TagLib::ID3v2::FrameList &frame,musik::core::Track *track);
+        void SetSlashSeparatedValues(const char* key,TagLib::String &tagString,musik::core::Track *track);
+
+        bool GetID3v2Tag(musik::core::Track *track);
+        bool GetGenericTag(musik::core::Track *track);
+        bool GetOGGTag(musik::core::Track *track);
 };
 
