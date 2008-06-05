ce5cd4206bfc818f044ee5148929dd3ca3c4d35d
Daniel Önnerby <onnerby@gmail.com>
2008-06-05 22:19:04 +0000

    Started making the metaKeys type in a more general way.

---
+++ src/core/MetaKey.cpp
@@ -0,0 +1,62 @@
+//////////////////////////////////////////////////////////////////////////////
+//
+// License Agreement:
+//
+// The following are Copyright © 2008, Daniel Önnerby
+//
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+//    * Redistributions of source code must retain the above copyright notice,
+//      this list of conditions and the following disclaimer.
+//
+//    * Redistributions in binary form must reproduce the above copyright
+//      notice, this list of conditions and the following disclaimer in the
+//      documentation and/or other materials provided with the distribution.
+//
+//    * Neither the name of the author nor the names of other contributors may
+//      be used to endorse or promote products derived from this software
+//      without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+//
+//////////////////////////////////////////////////////////////////////////////
+#include "pch.hpp"
+#include <core/MetaKey.h>
+#include <string>
+
+using namespace musik::core;
+
+MetaKey::MetaKey(void){
+}
+
+MetaKey::~MetaKey(void){
+}
+
+enum MetaKey::Type MetaKey::TypeOf(std::string metaKey){
+    if(metaKey=="bpm"){
+        return MetaKey::Float;
+    }else if(metaKey=="track" || metaKey=="year"){
+        return MetaKey::Int;
+    }else if(metaKey=="filetime"){
+        return MetaKey::Date;
+    }else if(metaKey=="duration"){
+        return MetaKey::Duration;
+    }else if(metaKey=="filesize"){
+        return MetaKey::Size;
+    }
+    return MetaKey::String;
+}
+
--- src/cube/TracklistColumn.cpp
+++ src/cube/TracklistColumn.cpp
@@ -1,11 +1,14 @@
 #include "pch.hpp"
 #include "TracklistColumn.hpp"
 
+#include <core/MetaKey.h>
+
 using namespace musik::cube;
 
-TracklistColumn::TracklistColumn(const char *metakey,const utfchar *name, int defaultWidth, TextAlignment alignment) : 
-    win32cpp::ListView::Column(name,defaultWidth,alignment),
-    metaKey(metakey)
+TracklistColumn::TracklistColumn(const char *metakey,const utfchar *name, int defaultWidth, TextAlignment alignment) 
+: win32cpp::ListView::Column(name,defaultWidth,alignment)
+, metaKey(metakey)
+, metaKeyType(musik::core::MetaKey::TypeOf(metakey))
 {
 }
 
