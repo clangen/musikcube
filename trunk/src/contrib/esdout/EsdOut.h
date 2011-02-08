953e217023b1e71f89e1f112689801f07f76a7db
urioxis <urioxis@6a861d04-ae47-0410-a6da-2d49beace72e>
2011-02-08 14:52:18 +0000

    Added esd output plugin

---
+++ trunk/src/contrib/esdout/EsdOut.h
@@ -0,0 +1,106 @@
+//////////////////////////////////////////////////////////////////////////////
+// Copyright © 2009, Julian Cromarty
+//
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+//    * Redistributions of source code must retain the above copyright notice,
+//      this list of conditions and the following disclaimer.
+//
+//    * Redistributions in binary form must reproduce the above copyright
+//      notice, this list of conditions and the following disclaimer in the
+//      documentation and/or other materials provided with the distribution.
+//
+//    * Neither the name of the author nor the names of other contributors may
+//      be used to endorse or promote products derived from this software
+//      without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+//
+//////////////////////////////////////////////////////////////////////////////
+#pragma once
+
+#include "pch.h"
+#include "EsdOutBuffer.h"
+/*
+#include <boost/thread/condition.hpp>
+#include <boost/thread/thread.hpp>
+
+#include <core/audio/IAudioCallback.h>
+#include <core/audio/IAudioOutput.h>
+*/
+#include <core/audio/IOutput.h>
+#include <list>
+#include <boost/shared_ptr.hpp>
+#include <boost/thread/mutex.hpp>
+
+using namespace musik::core::audio;
+
+class EsdOut : public IOutput{
+    public:
+        EsdOut();
+        ~EsdOut();
+
+        virtual void Destroy();
+        //virtual void Initialize(IPlayer *player);
+        virtual void Pause();
+        virtual void Resume();
+        virtual void SetVolume(double volume);
+        virtual void ClearBuffers();
+        virtual bool PlayBuffer(IBuffer *buffer,IPlayer *player);
+        virtual void ReleaseBuffers();
+        
+        int* getWaveHandle();
+
+    public:
+        typedef boost::shared_ptr<EsdOutBuffer> EsdOutBufferPtr;
+
+        //static void CALLBACK WaveCallback(HWAVEOUT hWave, UINT msg, DWORD_PTR dwUser, DWORD_PTR dw1, DWORD dw2);
+        void RemoveBuffer(EsdOutBuffer *buffer);
+
+    private:
+        void SetFormat(IBuffer *buffer);
+        char *device;                        /* playback device */
+
+    protected:
+        friend class EsdOutBuffer;
+
+        //IPlayer *player;
+
+        // Audio stuff
+        int *waveHandle;
+        //snd_pcm_t        *waveHandle;
+        esd_format_t waveFormat;
+        //snd_pcm_access_t waveAccess;
+        //snd_pcm_hw_params_t *hw_params;
+
+        /*int bits = ESD_BITS16, channels = ESD_STEREO;
+        int mode = ESD_STREAM, func = ESD_PLAY ;*/
+
+        // Current format
+        int currentChannels;
+        long currentSampleRate;
+        double currentVolume;
+
+        typedef std::list<EsdOutBufferPtr> BufferList;
+        BufferList buffers;
+        BufferList removedBuffers;
+        size_t maxBuffers;
+
+        boost::mutex mutex;
+
+        bool addToRemovedBuffers;
+
+};
---
+++ trunk/src/contrib/esdout/EsdOutBuffer.cpp
@@ -0,0 +1,109 @@
+//////////////////////////////////////////////////////////////////////////////
+// Copyright ﺏ 2007, Daniel ﺿnnerby
+//
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+//    * Redistributions of source code must retain the above copyright notice,
+//      this list of conditions and the following disclaimer.
+//
+//    * Redistributions in binary form must reproduce the above copyright
+//      notice, this list of conditions and the following disclaimer in the
+//      documentation and/or other materials provided with the distribution.
+//
+//    * Neither the name of the author nor the names of other contributors may
+//      be used to endorse or promote products derived from this software
+//      without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+//
+//////////////////////////////////////////////////////////////////////////////
+#include "EsdOutBuffer.h"
+#include "EsdOut.h"
+
+//////////////////////////////////////////////////////////////////////////////
+
+EsdOutBuffer::EsdOutBuffer(EsdOut *EsdOut,IBuffer *buffer,IPlayer *player)
+ :waveOut(EsdOut)
+ ,buffer(buffer)
+ ,player(player)
+{
+#ifdef _DEBUG
+	std::cerr << "EsdOutBuffer::EsdOutBuffer()" << std::endl;
+#endif
+    this->PrepareBuffer();
+}
+
+void EsdOutBuffer::PrepareBuffer(){
+#ifdef _DEBUG
+	std::cerr << "EsdOutBuffer::PrepareBuffer()" << std::endl;
+#endif
+	//unsigned float bufSize = this->buffer->Samples()*this->buffer->Channels()*sizeof(float);
+    /*// Prepare the header
+    this->header.dwBufferLength		= this->buffer->Samples()*this->buffer->Channels()*sizeof(float);
+    this->header.lpData				= (LPSTR)this->buffer->BufferPointer();
+	this->header.dwUser				= (DWORD_PTR)this;
+	this->header.dwBytesRecorded	= 0;
+	this->header.dwFlags			= 0;
+	this->header.dwLoops			= 0;
+    this->header.lpNext             = NULL;
+    this->header.reserved           = NULL;
+
+    MMRESULT result = waveOutPrepareHeader(this->waveOut->waveHandle,&this->header,sizeof(WAVEHDR));
+    if(result!=MMSYSERR_NOERROR){
+        throw;
+    }
+    this->header.dwFlags			|= WHDR_DONE;
+	*/
+	data = (char*)this->buffer->BufferPointer();
+	bufferLength = this->buffer->Samples()*this->buffer->Channels()*sizeof(float);
+			//TODO: Check data types.
+
+}
+
+EsdOutBuffer::~EsdOutBuffer(void)
+{
+#ifdef _DEBUG
+	std::cerr << "EsdOutBuffer::~EsdOutBuffer()" << std::endl;
+#endif
+    this->player->ReleaseBuffer(this->buffer);
+}
+
+bool EsdOutBuffer::AddToOutput(){
+#ifdef _DEBUG
+	std::cerr << "EsdOutBuffer::AddToOutput()" << std::endl;
+#endif
+    //int err;
+	int* wHandle = waveOut->getWaveHandle();
+	if (wHandle == NULL) {
+		printf("Error. No device handle \n");
+		return false;
+	}
+    /*frames = snd_pcm_writei(wHandle, (void*)data, bufferLength);
+                 if (frames < 0)
+                         frames = snd_pcm_recover(wHandle, frames, 0);
+                 if (frames < 0) {
+                         printf("snd_pcm_writei failed: %s\n", snd_strerror(err));
+                         return false;
+                 }
+                 if (frames > 0 && frames < (long)bufferLength)	{
+                         printf("Short write (expected %li, wrote %li)\n", (long)bufferLength, frames);
+                         return false;
+                 }*/
+	if ( write( *wHandle, *data, bufferLength ) <= 0 )
+		    return false;
+	return true;
+}
+
