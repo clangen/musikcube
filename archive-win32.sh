83d2acd47ac61e3ccbc1c8263f82c7c9ac0c016d
casey langen <casey.langen@gmail.com>
2017-02-24 20:57:07 -0800

    Updated archive-win32 script to also create the release zips.

--- archive-win32.sh
+++ archive-win32.sh
@@ -7,8 +7,8 @@ if [ -z "$VERSION" ]; then
   exit
 fi
 
-VANILLA="bin/dist/musikcube_win32_$VERSION"
-MILKDROP="bin/dist/musikcube_with_milkdrop2_win32_$VERSION"
+VANILLA="bin/dist/musikbox_win32_$VERSION"
+MILKDROP="bin/dist/musikbox_with_milkdrop2_win32_$VERSION"
 
 rm -rf "$VANILLA"
 rm -rf "$MILKDROP"
@@ -17,8 +17,16 @@ mkdir -p "$VANILLA/plugins"
 cp bin/release/musikbox.exe "$VANILLA" 
 cp bin/release/plugins/*.dll "$VANILLA/plugins"
 rm "$VANILLA/plugins/vis_milk2.dll"
+pushd $VANILLA
+7z a -tzip "musikbox_win32_$VERSION.zip" ./* -mx=9
+mv "musikbox_win32_$VERSION.zip" ..
+popd
 
 mkdir -p "$MILKDROP/plugins"
 cp bin/release/musikbox.exe "$MILKDROP"
 cp bin/release/plugins/*.dll "$MILKDROP/plugins"
 cp -rfp bin/release/plugins/Milkdrop2 "$MILKDROP/plugins"
+pushd $MILKDROP
+7z a -tzip "musikbox_with_milkdrop2_win32_$VERSION.zip" ./* -mx=9
+mv "musikbox_with_milkdrop2_win32_$VERSION.zip" ..
+popd
