1c7ed8ec268f2c7709a5e74378d955d7aff0b90c
casey langen <casey.langen@gmail.com>
2017-07-29 14:22:09 -0700

    Version bump for upcoming release: 0.22.0

--- src/musikcube/app/util/Version.h
+++ src/musikcube/app/util/Version.h
@@ -1,6 +1,6 @@
 #pragma once
 
 #define VERSION_MAJOR 0
-#define VERSION_MINOR 21
-#define VERSION_PATCH 1
-#define VERSION "0.21.1"
+#define VERSION_MINOR 22
+#define VERSION_PATCH 0
+#define VERSION "0.22.0"
