ae8016971348971246c744d6bf668cab9e36ef5d
casey langen <casey.langen@gmail.com>
2018-02-23 23:29:17 -0800

    Added theme support via #pragma comment so the font chooser and menus look nicer.

--- src/musikcube/Main.cpp
+++ src/musikcube/Main.cpp
@@ -66,6 +66,7 @@
     #include <cursespp/Win32Util.h>
     #include "resource.h"
     #undef MOUSE_MOVED
+    #pragma comment(linker,"/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
 #endif
 
 using namespace musik;
