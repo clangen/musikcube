0bde25edd36f915b7b8fc7bdff9ba85c2dd86dda
casey langen <casey.langen@gmail.com>
2018-08-25 17:02:09 -0700

    Preparing for 0.51.0 release

--- src/musikcube/app/version.h
+++ src/musikcube/app/version.h
@@ -3,9 +3,9 @@
 #include <string>
 
 #define VERSION_MAJOR 0
-#define VERSION_MINOR 50
+#define VERSION_MINOR 51
 #define VERSION_PATCH 0
-#define VERSION "0.50.0"
+#define VERSION "0.51.0"
 
 namespace musik {
     namespace cube {
