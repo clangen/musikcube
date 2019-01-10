e21e08340c95c70ef12d780b0d8b34799781c5c1
casey langen <casey.langen@gmail.com>
2019-01-09 19:48:41 -0800

    Version bump to 0.60.2

--- src/musikcube/app/version.h
+++ src/musikcube/app/version.h
@@ -4,8 +4,8 @@
 
 #define VERSION_MAJOR 0
 #define VERSION_MINOR 60
-#define VERSION_PATCH 1
-#define VERSION "0.60.1"
+#define VERSION_PATCH 2
+#define VERSION "0.60.2"
 
 namespace musik {
     namespace cube {
