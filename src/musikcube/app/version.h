771c41efc6299b731f15b4311a1d7083055b0979
casey langen <casey.langen@gmail.com>
2019-10-26 21:10:16 -0700

    Upgraded to 0.70.0 for upcoming release.

--- src/musikcube/app/version.h
+++ src/musikcube/app/version.h
@@ -3,9 +3,9 @@
 #include <string>
 
 #define VERSION_MAJOR 0
-#define VERSION_MINOR 65
-#define VERSION_PATCH 1
-#define VERSION "0.65.1"
+#define VERSION_MINOR 70
+#define VERSION_PATCH 0
+#define VERSION "0.70.0"
 
 namespace musik {
     namespace cube {
