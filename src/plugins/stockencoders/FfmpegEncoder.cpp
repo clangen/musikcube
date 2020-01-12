a9423e5d42b8916e2462a47bcbb0305c92f24a3d
casey langen <casey.langen@gmail.com>
2020-01-11 18:19:17 -0800

    Fixed MSVC compile.

--- src/plugins/stockencoders/FfmpegEncoder.cpp
+++ src/plugins/stockencoders/FfmpegEncoder.cpp
@@ -361,7 +361,9 @@ bool FfmpegEncoder::OpenOutputContext() {
 }
 
 bool FfmpegEncoder::Initialize(IDataStream* out, size_t rate, size_t channels, size_t bitrate) {
+#ifndef WIN32
     av_register_all();
+#endif
 
     this->out = out;
 
