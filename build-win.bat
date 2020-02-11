7d01d587a3c5d9e1755a1d065b844fca375804b9
casey langen <casey.langen@gmail.com>
2020-02-10 19:53:51 -0800

    Revert "Separate DLL targets for musikcore, and a couple build script tweaks."
    This reverts commit 351253fa5c972a5873b7781f2df65147bacf9dc8.

--- build-win.bat
+++ build-win.bat
@@ -1,16 +1,12 @@
 rd /s /q bin32\Release
 rd /s /q bin32\Release-Con
-rd /s /q bin32\Release-DLL
 rd /s /q bin64\Release
 rd /s /q bin64\Release-Con
-rd /s /q bin64\Release-DLL
 MSBuild.exe ../milkdrop2-musikcube/milkdrop2-musikcube.sln /m /nologo /verbosity:minimal /p:Configuration=Release /p:Platform=Win32
 MSBuild.exe musikcube.sln /m /nologo /verbosity:minimal /p:Configuration=Release-Win /p:Platform=Win32
 MSBuild.exe musikcube.sln /m /nologo /verbosity:minimal /p:Configuration=Release-Con /p:Platform=Win32
-MSBuild.exe musikcube.sln /m /nologo /verbosity:minimal /p:Configuration=Release-DLL /p:Platform=Win32
 MSBuild.exe musikcube.sln /m /nologo /verbosity:minimal /p:Configuration=Release-Win /p:Platform=Win64
 MSBuild.exe musikcube.sln /m /nologo /verbosity:minimal /p:Configuration=Release-Con /p:Platform=Win64
-MSBuild.exe musikcube.sln /m /nologo /verbosity:minimal /p:Configuration=Release-DLL /p:Platform=Win64
 copy bin32\Release-DLL\musikcore.dll bin32\Release\musikcore.dll
 copy bin32\Release-DLL\musikcore.lib bin32\Release\musikcore.lib
 copy bin32\Release-Con\musikcube.exe bin32\Release\musikcube-cmd.exe
