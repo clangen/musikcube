1c8b0c22d1fccad04463a0b122ec2251198fa0b0
casey langen <casey.langen@gmail.com>
2020-02-11 21:25:43 -0800

    More windows build script updates.

--- build-win.bat
+++ build-win.bat
@@ -1,13 +1,14 @@
-rd /s /q bin32\Release
-rd /s /q bin32\Release-Con
-rd /s /q bin32\Release-DLL
-rd /s /q bin64\Release
-rd /s /q bin64\Release-Con
-rd /s /q bin64\Release-DLL
+call clean-win.bat
+
+echo "*** BUILDING MILKDROP ***"
 MSBuild.exe ../milkdrop2-musikcube/milkdrop2-musikcube.sln /m /nologo /verbosity:minimal /p:Configuration=Release /p:Platform=Win32
+
+echo "*** BUILDING WIN32 ***"
 MSBuild.exe musikcube.sln /m /nologo /verbosity:minimal /p:Configuration=Release-Win /p:Platform=Win32
 MSBuild.exe musikcube.sln /m /nologo /verbosity:minimal /p:Configuration=Release-Con /p:Platform=Win32
 MSBuild.exe musikcube.sln /m /nologo /verbosity:minimal /p:Configuration=Release-DLL /p:Platform=Win32
+
+echo "*** BUILDING WIN64 ***"
 MSBuild.exe musikcube.sln /m /nologo /verbosity:minimal /p:Configuration=Release-Win /p:Platform=Win64
 MSBuild.exe musikcube.sln /m /nologo /verbosity:minimal /p:Configuration=Release-Con /p:Platform=Win64
 MSBuild.exe musikcube.sln /m /nologo /verbosity:minimal /p:Configuration=Release-DLL /p:Platform=Win64
