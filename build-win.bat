fdced4b5c09d8713f952da7a333d7d8b90848a6e
casey langen <casey.langen@gmail.com>
2020-02-15 14:11:19 -0800

    Added a bunch of cruft to time command line builds on Windows.

--- build-win.bat
+++ build-win.bat
@@ -1,3 +1,7 @@
+@echo off
+set start=%time%
+@echo on
+
 call clean-win.bat
 
 echo "*** BUILDING MILKDROP ***"
@@ -12,3 +16,22 @@ echo "*** BUILDING WIN64 ***"
 MSBuild.exe musikcube.sln /m /nologo /verbosity:minimal /p:Configuration=Release-Win /p:Platform=Win64
 MSBuild.exe musikcube.sln /m /nologo /verbosity:minimal /p:Configuration=Release-Con /p:Platform=Win64
 MSBuild.exe musikcube.sln /m /nologo /verbosity:minimal /p:Configuration=Release-DLL /p:Platform=Win64
+
+echo "*** DONE ***"
+@echo off
+set end=%time%
+set options="tokens=1-4 delims=:.,"
+for /f %options% %%a in ("%start%") do set start_h=%%a&set /a start_m=100%%b %% 100&set /a start_s=100%%c %% 100&set /a start_ms=100%%d %% 100
+for /f %options% %%a in ("%end%") do set end_h=%%a&set /a end_m=100%%b %% 100&set /a end_s=100%%c %% 100&set /a end_ms=100%%d %% 100
+set /a hours=%end_h%-%start_h%
+set /a mins=%end_m%-%start_m%
+set /a secs=%end_s%-%start_s%
+set /a ms=%end_ms%-%start_ms%
+if %ms% lss 0 set /a secs = %secs% - 1 & set /a ms = 100%ms%
+if %secs% lss 0 set /a mins = %mins% - 1 & set /a secs = 60%secs%
+if %mins% lss 0 set /a hours = %hours% - 1 & set /a mins = 60%mins%
+if %hours% lss 0 set /a hours = 24%hours%
+if 1%ms% lss 100 set ms=0%ms%
+set /a totalsecs = %hours%*3600 + %mins%*60 + %secs%
+@echo on
+echo Build time: %hours%:%mins%:%secs%.%ms% (%totalsecs%.%ms%s total)
