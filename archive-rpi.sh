e02845f2becc6af2062c9d03a7e0b85bbdf0bc60
casey langen <casey.langen@gmail.com>
2020-02-15 07:19:43 +0000

    Updated archive-rpi.sh script

--- archive-rpi.sh
+++ archive-rpi.sh
@@ -2,6 +2,6 @@
 
 rm -rf bin
 ./clean-nix.sh
-cmake -DGENERATE_DEB=1 -DDEB_ARCHITECTURE=armhf -DDEB_DISTRO=stretch -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Release .
+cmake -DGENERATE_DEB=1 -DDEB_ARCHITECTURE=armhf -DDEB_DISTRO=buster -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Release .
 make -j2
 cpack
