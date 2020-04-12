eeb1587c7897af895afbda267378cb18dfeb2b7b
casey langen <casey.langen@gmail.com>
2020-04-12 12:56:52 -0700

    Version bump for Win32 release.

--- src/musikcube/app/version.h
+++ src/musikcube/app/version.h
@@ -37,9 +37,9 @@
 #include <string>
 
 #define VERSION_MAJOR 0
-#define VERSION_MINOR 90
+#define VERSION_MINOR 91
 #define VERSION_PATCH 0
-#define VERSION "0.90.0"
+#define VERSION "0.91.0"
 namespace musik {
     namespace cube {
         static inline std::string userAgent() {
