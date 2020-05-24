836120f02903d4212121d824de6bddf2a2fbe906
casey langen <casey.langen@gmail.com>
2020-05-24 14:13:32 -0700

    Fixed bug where out of source tree builds were not getting stripped properly.

--- strip-nix.sh
+++ strip-nix.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
-strip ./bin/musikcube
-strip ./bin/musikcubed
-find ./bin/ -name "*.so" -exec strip "{}" \; 2> /dev/null
+DIR=$1
+if [ -z "$DIR" ]; then
+  echo "[strip] directory not specified, using current working directory"
+  DIR=`pwd`
+fi
+echo "[strip] resolved directory: ${DIR}"
+strip "$DIR/bin/musikcube"
+strip "$DIR/bin/musikcubed"
+find "$DIR/bin/" -name "*.so" -exec strip "{}" \; 2> /dev/null
+echo "[strip] finished"
