19634e67caae167448fc535aad9fb193ba00ab06
casey langen <casey.langen@gmail.com>
2020-06-19 02:47:55 +0100

    Version bump to 0.93.0 for next release.

--- src/musikcube/app/version.h
+++ src/musikcube/app/version.h
@@ -37,9 +37,9 @@
 #include <string>
 
 #define VERSION_MAJOR 0
-#define VERSION_MINOR 92
-#define VERSION_PATCH 1
-#define VERSION "0.92.1"
+#define VERSION_MINOR 93
+#define VERSION_PATCH 0
+#define VERSION "0.93.0"
 namespace musik {
     namespace cube {
         static inline std::string userAgent() {
