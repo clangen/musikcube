cc41b3a0cbdbeace51792ac8a8f3ac329209b84f
casey langen <casey.langen@gmail.com>
2020-10-08 19:58:31 -0700

    Oops, checked in a line I shouldn't have.

--- src/core/net/WebSocketClient.cpp
+++ src/core/net/WebSocketClient.cpp
@@ -244,8 +244,6 @@ void WebSocketClient::SendPendingQueries() {
             createSendRawQueryRequest(query->SerializeQuery(), messageId),
             websocketpp::frame::opcode::text);
     }
-
-    this->messageIdToQuery.clear();
 }
 
 void WebSocketClient::SetState(State state) {
