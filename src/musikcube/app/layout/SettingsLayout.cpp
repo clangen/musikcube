f6ce730a4f950dd8f15039c4ee13e73b2205c03b
Casey Langen <casey.langen@gmail.com>
2020-10-11 11:05:57 -0700

    Tighten up RemoteLibrarySettingsLayout

--- src/musikcube/app/layout/SettingsLayout.cpp
+++ src/musikcube/app/layout/SettingsLayout.cpp
@@ -334,7 +334,7 @@ void SettingsLayout::OnLayout() {
     }
     else {
         this->localLibraryLayout->Hide();
-        this->remoteLibraryLayout->MoveAndResize(2, 3, cx - 4, 6);
+        this->remoteLibraryLayout->MoveAndResize(2, 3, cx - 4, 5);
         libraryLayout = this->remoteLibraryLayout;
     }
     libraryLayout->Show();
