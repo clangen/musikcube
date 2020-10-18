b32d87de9266ceabd458b646f853f8db51a64af7
casey langen <casey.langen@gmail.com>
2020-10-18 06:24:29 +0100

    Upgrade to use 4 processes by default when building the rpi archive.

--- script/archive-rpi.sh
+++ script/archive-rpi.sh
@@ -5,5 +5,5 @@ SCRIPT_DIR=`dirname "$0"`
 rm -rf bin
 ${SCRIPT_DIR}/clean-nix.sh
 cmake -DGENERATE_DEB=1 -DDEB_ARCHITECTURE=armhf -DDEB_PLATFORM=rpi -DDEB_DISTRO=buster -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Release .
-make -j2
+make -j4
 cpack
