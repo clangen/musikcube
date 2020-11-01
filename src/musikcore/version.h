d23a6f84891f852afb545cb211b71b6155150b4b
casey langen <casey.langen@gmail.com>
2020-11-01 00:13:24 -0700

    version hash bump

--- src/musikcore/version.h
+++ src/musikcore/version.h
@@ -39,7 +39,7 @@
 #define VERSION_MAJOR 0
 #define VERSION_MINOR 94
 #define VERSION_PATCH 0
-#define VERSION_COMMIT_HASH "#5d2fd09c"
+#define VERSION_COMMIT_HASH "#04e8acc4"
 #define VERSION "0.94.0"
 
 namespace musik {
