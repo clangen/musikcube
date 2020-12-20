05ea062e03d1861b4fe0abf1b0e48682f6407375
Casey Langen <casey.langen@gmail.com>
2020-12-20 13:22:44 -0800

    Oops, version-bumped to the wrong version.

--- musikcube.spec
+++ musikcube.spec
@@ -1,6 +1,6 @@
 %define name musikcube
 %define build_timestamp %{lua: print(os.date("%Y%m%d"))}
-%define version 0.92.3
+%define version 0.96.3
 Name: %{name}
 Version: %{version}
 Release: %{dist}
--- src/musikcore/version.h
+++ src/musikcore/version.h
@@ -37,10 +37,10 @@
 #include <string>
 
 #define VERSION_MAJOR 0
-#define VERSION_MINOR 92
+#define VERSION_MINOR 96
 #define VERSION_PATCH 3
-#define VERSION_COMMIT_HASH "#0ca5bb4b"
-#define VERSION "0.92.3"
+#define VERSION_COMMIT_HASH "#b57c32a5"
+#define VERSION "0.96.3"
 
 namespace musik {
     namespace cube {
