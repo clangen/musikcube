c3ee55f89df08578ca28214d850f84dfb27809c6
Casey Langen <casey.langen@gmail.com>
2021-01-05 19:42:58 -0800

    Version bump and CHANGELOG update.

--- src/musikcore/version.h
+++ src/musikcore/version.h
@@ -38,9 +38,9 @@
 
 #define VERSION_MAJOR 0
 #define VERSION_MINOR 96
-#define VERSION_PATCH 3
-#define VERSION_COMMIT_HASH "#b445db57"
-#define VERSION "0.96.3"
+#define VERSION_PATCH 4
+#define VERSION_COMMIT_HASH "#3c77c21e"
+#define VERSION "0.96.4"
 
 namespace musik {
     namespace cube {
