3eee2471a8da33572a8e76a7a56de894eaca73e9
casey langen <casey.langen@gmail.com>
2021-01-16 11:16:42 -0800

    local circleci script fixes

--- script/copy-artifacts-to-host.sh
+++ script/copy-artifacts-to-host.sh
@@ -8,5 +8,4 @@ if [[ -z "${MUSIKCUBE_BUILD_HOST_PW}" ]]; then
     exit
 fi
 
-sshpass -p "${MUSIKCUBE_BUILD_HOST_PW}" scp /root/rpms/* build@${MUSIKCUBE_BUILD_HOST_IP}:/home/build/ 2> /dev/null
-sshpass -p "${MUSIKCUBE_BUILD_HOST_PW}" scp /root/debs/* build@${MUSIKCUBE_BUILD_HOST_IP}:/home/build/ 2> /dev/null
+sshpass -p ${MUSIKCUBE_BUILD_HOST_PW} scp "$1/*" build@${MUSIKCUBE_BUILD_HOST_IP}:/home/build/ 2> /dev/null
