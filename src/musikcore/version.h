d1d9db1e5383e3a661604f50c1bfb6212fb20ae7
casey langen <casey.langen@gmail.com>
2021-12-28 13:48:07 -0800

    Update version hash.

--- src/musikcore/version.h
+++ src/musikcore/version.h
@@ -39,7 +39,7 @@
 #define VERSION_MAJOR 0
 #define VERSION_MINOR 96
 #define VERSION_PATCH 10
-#define VERSION_COMMIT_HASH "#18960811"
+#define VERSION_COMMIT_HASH "#6c1ae05b"
 #define VERSION "0.96.10"
 
 namespace musik {
