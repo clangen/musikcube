332e8c25d10d0b4e3e093b78630ab1300f127c73
casey langen <casey.langen@gmail.com>
2022-01-02 22:12:45 -0800

    Fix duration calculation for float-encoded WAVs.
    https://github.com/taglib/taglib/commit/e4813f4996d788e0a4c5b8a6fd495b30eca24a9e

--- src/plugins/taglib_plugin/taglib-1.11/taglib/riff/wav/wavproperties.cpp
+++ src/plugins/taglib_plugin/taglib-1.11/taglib/riff/wav/wavproperties.cpp
@@ -35,7 +35,8 @@ namespace
   enum WaveFormat
   {
     FORMAT_UNKNOWN = 0x0000,
-    FORMAT_PCM     = 0x0001
+    FORMAT_PCM     = 0x0001,
+    FORMAT_IEEE_FLOAT = 0x0003
   };
 }
 
@@ -183,7 +184,7 @@ void RIFF::WAV::Properties::read(File *file)
   }
 
   d->format = data.toShort(0, false);
-  if(d->format != FORMAT_PCM && totalSamples == 0) {
+  if(d->format != FORMAT_PCM && d->format != FORMAT_IEEE_FLOAT && totalSamples == 0) {
     debug("RIFF::WAV::Properties::read() - Non-PCM format, but 'fact' chunk not found.");
     return;
   }
@@ -192,7 +193,7 @@ void RIFF::WAV::Properties::read(File *file)
   d->sampleRate    = data.toUInt(4, false);
   d->bitsPerSample = data.toShort(14, false);
 
-  if(d->format != FORMAT_PCM)
+  if (d->format != FORMAT_PCM && !(d->format == FORMAT_IEEE_FLOAT && totalSamples == 0))
     d->sampleFrames = totalSamples;
   else if(d->channels > 0 && d->bitsPerSample > 0)
     d->sampleFrames = streamLength / (d->channels * ((d->bitsPerSample + 7) / 8));
