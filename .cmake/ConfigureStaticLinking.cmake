e357a1fd9d50967bae96ac8fab60228fe7227174
casey langen <casey.langen@gmail.com>
2022-02-07 19:53:26 -0800

    More work towards static linking. Things seme to work except the server.

--- .cmake/ConfigureStaticLinking.cmake
+++ .cmake/ConfigureStaticLinking.cmake
@@ -5,7 +5,7 @@ if (${LINK_STATICALLY} MATCHES "true")
   set(BOOST_INCLUDEDIR "${CMAKE_CURRENT_SOURCE_DIR}/vendor/boost-bin/include")
   set(BOOST_LIBRARYDIR "${CMAKE_CURRENT_SOURCE_DIR}/vendor/boost-bin/lib")
   set(Boost_NO_SYSTEM_PATHS ON)
-  set(Boost_USE_STATIC_LIBS ON)
+  #set(Boost_USE_STATIC_LIBS ON)
   set(Boost_NO_BOOST_CMAKE ON)
 
   include_directories("${CMAKE_CURRENT_SOURCE_DIR}/vendor/ffmpeg-bin/include")
@@ -19,6 +19,9 @@ if (${LINK_STATICALLY} MATCHES "true")
 
   include_directories("${CMAKE_CURRENT_SOURCE_DIR}/vendor/zlib-bin/include")
   link_directories("${CMAKE_CURRENT_SOURCE_DIR}/vendor/zlib-bin/lib")
+
+  include_directories("${CMAKE_CURRENT_SOURCE_DIR}/vendor/curl-bin/include")
+  link_directories("${CMAKE_CURRENT_SOURCE_DIR}/vendor/curl-bin/lib")
 else()
     message(STATUS "[static linking] static linking NOT enabled!")
 endif()
--- script/bootstrap-static-vendors.sh
+++ script/bootstrap-static-vendors.sh
@@ -233,3 +233,14 @@ make install
 mv output ../zlib-bin
 cd ..
 
+#
+# stage
+#
+
+mkdir -p bin/plugins/
+cp vendor/boost-bin/lib/libboost_filesystem.so.1.78.0 ./bin/
+cp vendor/boost-bin/lib/libboost_thread.so.1.78.0 ./bin/
+cp vendor/ffmpeg-bin/lib/libavcodec-musikcube.so.59 ./bin/plugins/
+cp vendor/ffmpeg-bin/lib/libavformat-musikcube.so.59 ./bin/plugins
+cp vendor/ffmpeg-bin/lib/libavutil-musikcube.so.57 ./bin/plugins
+cp vendor/ffmpeg-bin/lib/libswresample-musikcube.so.4 ./bin/plugins
