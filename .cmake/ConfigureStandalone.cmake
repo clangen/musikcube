66305dcc3f94ccd6a9ffa7cf6b8bccda95046bc1
casey langen <casey.langen@gmail.com>
2022-02-17 10:55:32 -0800

    More path fixes.

--- .cmake/ConfigureStandalone.cmake
+++ .cmake/ConfigureStandalone.cmake
@@ -1,9 +1,9 @@
 if (${BUILD_STANDALONE} MATCHES "true")
   message(STATUS "[standalone-build] ENABLED!")
 
-  set(BOOST_ROOT "${CMAKE_CURRENT_SOURCE_DIR}/vendor/bin")
-  set(BOOST_INCLUDEDIR "${CMAKE_CURRENT_SOURCE_DIR}/bin/include")
-  set(BOOST_LIBRARYDIR "${CMAKE_CURRENT_SOURCE_DIR}/bin/lib")
+  set(BOOST_ROOT "/build/musikcube/vendor/bin")
+  set(BOOST_INCLUDEDIR "${CMAKE_CURRENT_SOURCE_DIR}/vendor/bin/include")
+  set(BOOST_LIBRARYDIR "${CMAKE_CURRENT_SOURCE_DIR}/vendor/bin/lib")
   set(Boost_NO_SYSTEM_PATHS ON)
   set(Boost_NO_BOOST_CMAKE ON)
 
