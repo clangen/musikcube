25a22ffc3194ad059e8f76aa8ad4c8c1cba58520
casey langen <casey.langen@gmail.com>
2022-12-22 23:20:34 -0800

    CHANGELOG update.

--- src/musikcore/version.h
+++ src/musikcore/version.h
@@ -39,7 +39,7 @@
 #define VERSION_MAJOR 0
 #define VERSION_MINOR 99
 #define VERSION_PATCH 1
-#define VERSION_COMMIT_HASH "#1a39eecb"
+#define VERSION_COMMIT_HASH "#7b5d8242"
 #define VERSION "0.99.1"
 
 namespace musik {
