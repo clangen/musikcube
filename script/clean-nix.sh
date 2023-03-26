6aa6a21b67f801e4b3ddb365d6287107593492f6
casey langen <casey.langen@gmail.com>
2023-03-25 17:29:38 -0700

    Remove build directory as well.

--- script/clean-nix.sh
+++ script/clean-nix.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 make clean 2> /dev/null
-rm -rf bin
+rm -rf bin 2> /dev/null
+rm -rf build 2> /dev/null
 find . -name CMakeCache.txt -delete
 find . -name CMakeFiles -type d -exec rm -rf "{}" \; 2> /dev/null
 rm -f Makefile
