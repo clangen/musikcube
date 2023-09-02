a0b5003690ff392e5e9d2db1b2fffe8a7aa99c80
casey langen <casey.langen@gmail.com>
2023-09-01 17:29:56 -0700

    Ensure we support OpenSSL 1.1 and 3.0 libraries when arching standalone builds.

--- script/stage-vendor-libraries.sh
+++ script/stage-vendor-libraries.sh
@@ -36,8 +36,10 @@ elif [[ "$PLATFORM" == 'Linux' ]]; then
     cp vendor/bin/lib/libavformat-musikcube.so.60 ./bin/lib
     cp vendor/bin/lib/libavutil-musikcube.so.58 ./bin/lib
     cp vendor/bin/lib/libswresample-musikcube.so.4 ./bin/lib
-    cp vendor/bin/lib/libcrypto.so.3 ./bin/lib
-    cp vendor/bin/lib/libssl.so.3 ./bin/lib
+    cp vendor/bin/lib/libcrypto.so.3 ./bin/lib 2> /dev/null
+    cp vendor/bin/lib/libssl.so.3 ./bin/lib 2> /dev/null
+    cp vendor/bin/lib/libcrypto.so.1.1 ./bin/lib 2> /dev/null
+    cp vendor/bin/lib/libssl.so.1.1 ./bin/lib 2> /dev/null
     cp vendor/bin/lib/libcurl.so.4 ./bin/lib
     cp vendor/bin/lib/libmp3lame.so.0 ./bin/lib
     cp vendor/bin/lib/libmicrohttpd.so.12 ./bin/lib
