cf8746041b4c23e4b3f3a2f9cea9fa8c6b9fea06
casey langen <casey.langen@gmail.com>
2023-09-04 01:46:08 -0700

    Update (still untested) armv7a toolchain file

--- .cmake/RaspberryPiToolchain-armv7a.cmake
+++ .cmake/RaspberryPiToolchain-armv7a.cmake
@@ -1,17 +1,4 @@
-# set(CMAKE_SYSTEM_NAME Linux)
-# set(CMAKE_SYSTEM_PROCESSOR armv7-a)
-
-# set(CROSS_COMPILE_SYSROOT /build/rpi-armv7a/sysroot)
-# set(CROSS_COMPILE_PKG_CONFIG_PATH "${CROSS_COMPILE_SYSROOT}/usr/lib/arm-linux-gnueabihf/pkgconfig")
-
-# set(CMAKE_SYSROOT ${CROSS_COMPILE_SYSROOT})
-
-# set(CMAKE_C_COMPILER arm-linux-gnueabihf-gcc)
-# set(CMAKE_CXX_COMPILER arm-linux-gnueabihf-g++)
-
-# set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
-# set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
-# set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
-# set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
-
 include("/build/x-tools/armv8-rpi3-linux-gnueabihf/armv8-rpi3-linux-gnueabihf.toolchain.cmake")
+set(CROSS_COMPILE_SYSROOT /build/x-tools/armv8-rpi3-linux-gnueabihf/armv8-rpi3-linux-gnueabihf/sysroot)
+set(CROSS_COMPILE_PKG_CONFIG_PATH "${CROSS_COMPILE_SYSROOT}/usr/lib/arm-linux-gnueabi/pkgconfig")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -static-libstdc++")
